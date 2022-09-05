3323f89027fd70a0cffbb9aa4872b1b34ba6aac4
Hunter-Thompson <aatman@auroville.org.in>
2022-09-05 22:08:14 +0530

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,25 @@
+const { cdk8s } = require('projen');
+const project = new cdk8s.ConstructLibraryCdk8s({
+  author: 'Hunter-Thompson',
+  authorAddress: 'aatman@auroville.org.in',
+  cdk8sVersion: '2.2.74',
+  constructsVersion: '10.0.5',
+  defaultReleaseBranch: 'main',
+  stability: 'experimental',
+  name: '@opencdk8s/cdk8s-external-secrets',
+  repositoryUrl: 'https://github.com/opencdk8s/cdk8s-external-secrets.git',
+  publishToGo: {
+    gitUserName: 'Hunter-Thompson',
+    gitUserEmail: 'aatman@auroville.org.in',
+    moduleName: 'github.com/opencdk8s/cdk8s-external-secrets-go',
+  },
+  npmAccess: 'public',
+  keywords: ['cdk8s'],
+
+  depsUpgrade: false,
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
